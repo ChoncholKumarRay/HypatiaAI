573250e02868862aa62cb194fe43d8739f76c5bd
ChoncholKumarRay <fusitivechonchol@gmail.com>
2024-06-08 08:02:23 +0600

    adding react-icon

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaFacebookF, FaYoutube, FaTwitter } from "react-icons/fa";
 import "./Footer.css";
 
 const Footer = () => {
@@ -15,27 +16,33 @@ const Footer = () => {
           </div>
           <div className="footer-section social-links">
             <h2>Follow Us</h2>
-            <a
-              href="https://www.facebook.com/camsust"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src="/path/to/facebook-icon.svg" alt="Facebook" />
-            </a>
-            <a
-              href="https://www.youtube.com/@cam-sust7650"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src="/path/to/youtube-icon.svg" alt="YouTube" />
-            </a>
-            <a
-              href="https://x.com/camsust"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src="/path/to/twitter-icon.svg" alt="Twitter" />
-            </a>
+            <div className="social-icon">
+              <a
+                href="https://www.facebook.com/camsust"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaFacebookF />
+              </a>
+            </div>
+            <div className="social-icon">
+              <a
+                href="https://www.youtube.com/@cam-sust7650"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaYoutube />
+              </a>
+            </div>
+            <div className="social-icon">
+              <a
+                href="https://x.com/camsust"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaTwitter />
+              </a>
+            </div>
           </div>
           <div className="footer-section developer">
             <h2>Developer</h2>
